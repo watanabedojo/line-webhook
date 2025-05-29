c7b39d9b45caaf8fa44e654ecc4a755230ec5bcf
watanabedojo <info@watanabedojo.jp>
2025-05-29 11:19:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// チャネルアクセストークン（環境変数 or 直書きテスト用）
+// チャネルアクセストークン（固定で貼り付け済）
 const LINE_CHANNEL_ACCESS_TOKEN = 'GHpLdoUF9qwf0mkDGCbqigN7Fkuhai9jGUMdd1I5iJGy8ICkv1Jjmvm8UITjIg7GzlO6LkGIMb6wJYBGFyflXZoy3IC8mtZ1mOSO7GMo/rwO4J1WMIY0t7l+MCBa9yEHhKZ7kvwp4wAy5lua/cI4WgdB04t89/1O/w1cDnyilFU=';
 
-// LINEユーザーID（テスト用に取得済みのものを貼り付け）
-const TEST_USER_ID = '<<ここにユーザーID>>';
+// テスト送信用ユーザーID（取得済み）
+const TEST_USER_ID = 'U5cb571e2ad5cfbcfdda8f21e5ded2f0a';
 
 app.get('/test', async (req, res) => {
   try {
