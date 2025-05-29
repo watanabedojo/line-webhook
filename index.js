b23fdeffc19bd8b6e99c54c51678a252d856f51c
watanabedojo <info@watanabedojo.jp>
2025-05-29 11:36:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 // チャネルアクセストークン（固定で貼り付け済）
 const LINE_CHANNEL_ACCESS_TOKEN = 'GHpLdoUF9qwf0mkDGCbqigN7Fkuhai9jGUMdd1I5iJGy8ICkv1Jjmvm8UITjIg7GzlO6LkGIMb6wJYBGFyflXZoy3IC8mtZ1mOSO7GMo/rwO4J1WMIY0t7l+MCBa9yEHhKZ7kvwp4wAy5lua/cI4WgdB04t89/1O/w1cDnyilFU=';
 
-// テスト送信用ユーザーID（取得済み）
+// テスト送信用ユーザーID
 const TEST_USER_ID = 'U5cb571e2ad5cfbcfdda8f21e5ded2f0a';
 
 app.get('/test', async (req, res) => {
@@ -18,7 +18,7 @@ app.get('/test', async (req, res) => {
       messages: [
         {
           type: 'text',
-          text: '📢 テストメッセージ送信成功！',
+          text: '📢 テストメッセージ送信成功！'
         }
       ]
     }, {
